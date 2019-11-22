1bc7043d175e8211050bb3bd54395176f009ce9d
Biligt <d.biligt@gmail.com>
2019-11-22 09:38:09 +0800

    Roll dice

--- appp.js
+++ appp.js
@@ -1,6 +1,12 @@
 var activePlayer = 0;
 var scores = [0, 0];
 var roundScore = 0;
-var dice = Math.floor(Math.random() * 6 + 1);
+var diceNumber = Math.floor(Math.random() * 6 + 1);
 window.document.querySelector("#score-0").textContent = dice;
 document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  roundScore = +diceNumber;
+  document.querySelector(".dice").style.display = "block";
+  document.querySelector(".dice").src = "dice-" + diceNumber;
+  // console.log(Math.floor(Math.random() * 6 + 1));
+});
