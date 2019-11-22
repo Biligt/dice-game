536782d8550b27d36c48e14764f6496627a1552e
Biligt <d.biligt@gmail.com>
2019-11-22 14:47:25 +0800

    Тоглоомыг дуусгадаг болсон

--- appp.js
+++ appp.js
@@ -1,5 +1,5 @@
 var activePlayer = 0;
-var scores = [0, 0];
+var scores = [99, 0];
 var roundScore = 0;
 
 document.getElementById("score-0").textContent = 0;
@@ -24,13 +24,32 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   } else {
     roundScore = 0;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.toggle("active");
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.toggle("active");
+    switchPlayer();
     diceDOM.style.display = "none";
   }
 });
+
+//HOLD
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+    document.getElementById("name-" + activePlayer).textContent = "Winner!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else switchPlayer();
+  roundScore = 0;
+});
+
+function switchPlayer() {
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  diceDOM.style.display = "none";
+}
