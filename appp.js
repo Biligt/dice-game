eedf7ae3e9b59364452f5a3cd60d73f28022cc93
Biligt <d.biligt@gmail.com>
2019-11-22 15:08:14 +0800

    finished

--- appp.js
+++ appp.js
@@ -1,11 +1,15 @@
 var activePlayer = 0;
 var scores = [99, 0];
 var roundScore = 0;
+var isOver = true;
 
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
+document.querySelector(".btn-new").addEventListener("click", function() {
+  document.getElementById("score-0").textContent = 0;
+  document.getElementById("score-1").textContent = 0;
+  document.getElementById("current-0").textContent = 0;
+  document.getElementById("current-1").textContent = 0;
+  isOver = false;
+});
 
 // window.document.querySelector("#score-0").textContent = dice;
 var diceDOM = document.querySelector(".dice");
@@ -14,37 +18,46 @@ diceDOM.style.display = "none";
 
 // Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  var diceNumber = Math.floor(Math.random() * 6 + 1); // get random number (1-6)
-  diceDOM.style.display = "block";
-  diceDOM.src = "dice-" + diceNumber + ".png";
-  // console.log(Math.floor(Math.random() * 6 + 1));
-  if (diceNumber !== 1) {
-    roundScore = roundScore + diceNumber;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
-  } else {
-    roundScore = 0;
-    document.getElementById("current-" + activePlayer).textContent = roundScore;
-    switchPlayer();
-    diceDOM.style.display = "none";
-  }
+  if (!isOver) {
+    var diceNumber = Math.floor(Math.random() * 6 + 1); // get random number (1-6)
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + diceNumber + ".png";
+    // console.log(Math.floor(Math.random() * 6 + 1));
+    if (diceNumber !== 1) {
+      roundScore = roundScore + diceNumber;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      roundScore = 0;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+      switchPlayer();
+      diceDOM.style.display = "none";
+    }
+  } else alert("Game is over! \nPress NEW GAME");
 });
 
 //HOLD
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  scores[activePlayer] = scores[activePlayer] + roundScore;
-  document.getElementById("score-" + activePlayer).textContent =
-    scores[activePlayer];
+  if (!isOver) {
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+    document.getElementById("score-" + activePlayer).textContent =
+      scores[activePlayer];
 
-  if (scores[activePlayer] >= 100) {
-    document.getElementById("name-" + activePlayer).textContent = "Winner!";
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.add("winner");
-    document
-      .querySelector(".player-" + activePlayer + "-panel")
-      .classList.remove("active");
-  } else switchPlayer();
-  roundScore = 0;
+    if (scores[activePlayer] >= 100) {
+      document.getElementById("name-" + activePlayer).textContent = "Winner!";
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.add("winner");
+      document
+        .querySelector(".player-" + activePlayer + "-panel")
+        .classList.remove("active");
+      isOver = true;
+    } else switchPlayer();
+    roundScore = 0;
+  } else alert("Game is over! \nPress NEW GAME");
 });
 
 function switchPlayer() {
