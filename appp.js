4ff1393e62c260433767f44b1aa9fff017821777
Biligt <d.biligt@gmail.com>
2019-11-22 10:15:51 +0800

    git status

--- appp.js
+++ appp.js
@@ -1,12 +1,36 @@
 var activePlayer = 0;
 var scores = [0, 0];
 var roundScore = 0;
-var diceNumber = Math.floor(Math.random() * 6 + 1);
-window.document.querySelector("#score-0").textContent = dice;
-document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+// window.document.querySelector("#score-0").textContent = dice;
+var diceDOM = document.querySelector(".dice");
+
+diceDOM.style.display = "none";
+
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  roundScore = +diceNumber;
-  document.querySelector(".dice").style.display = "block";
-  document.querySelector(".dice").src = "dice-" + diceNumber;
+  var diceNumber = Math.floor(Math.random() * 6 + 1); // get random number (1-6)
+  diceDOM.style.display = "block";
+  diceDOM.src = "dice-" + diceNumber + ".png";
   // console.log(Math.floor(Math.random() * 6 + 1));
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("active");
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.toggle("active");
+    diceDOM.style.display = "none";
+  }
 });
